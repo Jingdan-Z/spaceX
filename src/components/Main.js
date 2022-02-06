88029ac2df7c23e11b91c0b37102a55dd7eb3b01
Danni <zhangjingdan222@gmail.com>
2022-02-06 17:54:14 -0500

    init commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,57 @@
 import React, {Component} from 'react';
 import { Row, Col } from 'antd';
+import axios from 'axios';
 import SatSetting from './SatSetting';
+import SatelliteList from './SatelliteList';
+import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constants";
 
 class Main extends Component {
+    constructor(){
+        super();
+        this.state = {
+            satInfo: null,
+            settings: null,
+            isLoadingList: false
+        };
+    }
+
+    showNearbySatellite = (setting) => {
+        this.setState({
+            settings: setting
+        })
+        this.fetchSatellite(setting);
+    }
+
+    fetchSatellite= (setting) => {
+        const {latitude, longitude, elevation, altitude} = setting;
+        const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+
+   this.setState({
+       isLoadingList: true
+   });
+
+   axios.get(url)
+       .then(response => {
+           console.log(response.data)
+           this.setState({
+               satInfo: response.data,
+               isLoadingList: false
+           })
+       })
+       .catch(error => {
+           console.log('err in fetch satellite -> ', error);
+       })
+}
+
     render() {
+        const { satInfo, isLoadingList } = this.state;
         return (
             <Row className='main'>
-                <Col span={8} className="left-side">
-                    <SatSetting />
+                <Col span={8} className='left-side'>
+                    <SatSetting onShow={this.showNearbySatellite}/>
+                    <SatelliteList satInfo={satInfo}
+                                   isLoad={isLoadingList}
+                    />
                 </Col>
                 <Col span={16} className="right-side">
                     right
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const SAT_API_KEY = "KSG7P2-K2TL5R-VYMYXU-4U9V";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
